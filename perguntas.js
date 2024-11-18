ab958ff05ee522c26fe01fe19f71c01daf072091
rafael00721 <166011004+rafael00721@users.noreply.github.com>
2024-11-18 07:10:50 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Nomes ',
+    'Qual o significado do nome moisés?',
+    'Aquele q foi retirado das águsas'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é brásilia'
+)
+
+criaCartao(
+    'Matemática',
+    'quanto é 50+60?',
+    '50+60 é 110'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz boa noite em Inglês?',
+    'Boa noite  em ingles é good night'
+)
